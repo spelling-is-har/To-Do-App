45d04349b5519babfb8371ed385ad43f0810d9e2
Jonathan Winter <jonathan.winter@hotmail.com>
2026-02-20 12:19:34 +0000

    Add check to ensure task was not in project.tasks
    Added a check to ensure that when adding a new task to a project that
    it was not already a part of that project, using the id as the check.
    
    On branch main
    Your branch is ahead of 'origin/main' by 3 commits.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	modified:   addTaskToProject.js
    
    Untracked files:
    	../.gitignore

--- src/addTaskToProject.js
+++ src/addTaskToProject.js
@@ -6,6 +6,11 @@ export function addTaskToProject(task, project) {
   if (!project) {
     throw new Error("Project not defined");
   }
+
+  //check to ensure that the task is not already a part of this project
+  const found = project.tasks.find(({ id }) => id === task.id);
+  if (found) throw new Error("This task is already in the project");
+
   //temporary array for the return value
   let updatedTask = project.tasks;
   updatedTask.push(task);
