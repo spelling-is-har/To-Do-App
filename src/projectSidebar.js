4b80d248013c04f45923004d3b1a520179804ae9
Jonathan Winter <jonathan.winter@hotmail.com>
2026-02-24 20:48:32 +0000

    commit

--- src/projectSidebar.js
+++ src/projectSidebar.js
@@ -1,10 +1,12 @@
 import { displayProject } from "./displayProject.js";
-import { retrieveProject } from "./localStorage.js";
+import { retrieveProject, saveProject } from "./localStorage.js";
+import { Project } from "./project.js";
 
 export function buildProjectNav() {
   const projectKeys = Object.keys(localStorage);
 
   const projectNav = document.querySelector(".projects-nav");
+  projectNav.innerHTML = "";
   for (let projectKey of projectKeys) {
     const project = retrieveProject(projectKey);
 
@@ -27,15 +29,26 @@ newProjectButton.addEventListener("click", (event) => {
   const newProjectForm = document.querySelector("#new-project-form");
   newProjectForm.addEventListener("submit", (event) => {
     event.preventDefault();
+
     const name = document.querySelector("#new-project-name").value;
     const date = document.querySelector("#new-project-date").value;
     const description = document.querySelector(
       "#new-project-description",
     ).value;
 
-    console.log(date);
+    const newProject = new Project(name, date, description);
+    console.log(newProject);
+
+    saveProject(newProject);
+    buildProjectNav();
+
+    document.querySelector("#new-project-name").value = "";
+    document.querySelector("#new-project-date").value = "";
+    document.querySelector("#new-project-description").value = "";
 
     const dialog = document.querySelector("#new-project-dialog");
     dialog.close();
   });
 });
+
+//button for deleting a project
